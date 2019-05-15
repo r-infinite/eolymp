0e5e899b0c1ad110dd1cafc4d24eb94da10dc41d
r-infinite <38298043+r-infinite@users.noreply.github.com>
2019-05-15 14:43:24 +0400

    Update eo3603.cs

--- eo3603.cs
+++ eo3603.cs
@@ -10,15 +10,13 @@ namespace OlimpSharp
             int count = 0;
             int[] arr = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 };
 
-            while (true)
+            while (n != 0)
             {
                 int nlog = (int)Math.Round(Math.Log(n, 2));
 
                 n = Math.Abs(arr[nlog] - n);
 
                 count++;
-
-                if (n == 0) break;
             }
 
             Console.WriteLine(count);
